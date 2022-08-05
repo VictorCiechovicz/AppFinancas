218740e8b2c44a58a672cd0d578cac1be688fa7f
victor ciechovicz <victor09ciechovicz@outlook.com>
2022-08-04 21:11:28 -0300

    upgrade

---
+++ Componentes/Actions/estilos.js
@@ -0,0 +1,29 @@
+import {StyleSheet} from 'react-native'
+
+const estilos = StyleSheet.create({
+  container: {
+    maxHeight: 84,
+    marginBottom: 14,
+    marginTop: 18,
+    paddingEnd: 14,
+    paddingStart: 14
+  },
+  actionButton: {
+    alignItems: 'center',
+    marginRight: 32
+  },
+  areaButton: {
+    backgroundColor: '#ecf0f1',
+    width: 60,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 60 / 2
+  },
+  labelButton: {
+    marginTop: 4,
+    textAlign: 'center',
+    fontWeight: 'bold'
+  }
+})
+export default estilos
--- Componentes/Actions/index.js
+++ Componentes/Actions/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {
   View,
   Text,
-  StyleSheet,
   TouchableOpacity,
   ScrollView
 } from 'react-native'
@@ -54,29 +53,3 @@ export default function Actions() {
   )
 }
 
-const estilos = StyleSheet.create({
-  container: {
-    maxHeight: 84,
-    marginBottom: 14,
-    marginTop: 18,
-    paddingEnd: 14,
-    paddingStart: 14
-  },
-  actionButton: {
-    alignItems: 'center',
-    marginRight: 32
-  },
-  areaButton: {
-    backgroundColor: '#ecf0f1',
-    width: 60,
-    height: 60,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 60 / 2
-  },
-  labelButton: {
-    marginTop: 4,
-    textAlign: 'center',
-    fontWeight: 'bold'
-  }
-})
